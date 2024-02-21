c5c19afd61a23e1bc9015720eb45d2129914381b
Arseny (HP) <senya.groznetscky@gmail.com>
2024-02-22 00:54:16 +0300

    V3: only vertical and horizontal edge

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ import time
 
 def make_sparse_laplacian(lab, rad=1, activate_value=15):
     h, w = np.shape(lab)[:2]
-    cnt_not_zeroes = 2 * ((2 * rad + 1) ** 2 - 1) * (w - 2 * rad) * (h - 2 * rad)
+    cnt_not_zeroes = ((2 * rad + 1) ** 2 - 1) * (w - 2 * rad) * (h - 2 * rad)
 
     data = np.zeros(cnt_not_zeroes)
     row = np.zeros(cnt_not_zeroes)
     col = np.zeros(cnt_not_zeroes)
 
     it = 0
-    for i in range(rad, h - rad):
-        for j in range(rad, w - rad):
-            for k in range(2 * rad + 1):
-                for p in range(2 * rad + 1):
-                    if k == rad and p == rad:
+    for i in range(0, h - rad):
+        for j in range(0, w - rad):
+            for k in range(rad, 2 * rad + 1):
+                for p in range(rad, 2 * rad + 1):
+                    if k == p:
                         continue
                     color_diff = ((lab[i - rad + k][j - rad + p][0] - lab[i][j][0]) ** 2 +
                                   (lab[i - rad + k][j - rad + p][1] - lab[i][j][1]) ** 2 +
@@ -32,14 +32,12 @@ def make_sparse_laplacian(lab, rad=1, activate_value=15):
 
                     row[it] = i * w + j
                     col[it] = (i - rad + k) * w + (j - rad + p)
-                    data[it] = 1 if ((i - rad + k) < rad or (i - rad + k) > h - 1 - rad
-                                     or (j - rad + p) < rad or (j - rad + p) > w - 1 - rad) \
-                        else 0.5
+                    data[it] = 1
                     it += 1
 
                     row[it] = col[it - 1]
                     col[it] = row[it - 1]
-                    data[it] = data[it - 1]
+                    data[it] = 1
                     it += 1
 
     adjacency_matrix = sparse.csr_matrix((data, (row, col)), shape=(w * h, w * h))
@@ -72,9 +70,8 @@ def save_concat_image(filename, fiedler_vector, dt, act_val, alpha=0.15):
     comb_image[:, :, :, ] = (alpha * rgb[:, :, :, ] / 256) + ((1 - alpha) * dst[:, :, :, ] / 256)
 
     im = Image.fromarray((comb_image * 255).astype(np.uint8))
-    im.save('Res/av-' + str(act_val) + '___now_time-' + str(time.time()).split('.')[0] + '___exec_time-' + str(dt)[
-                                                                                                           :5] + "sec___" +
-            filename.split('\\')[1])
+    im.save('Res/av-' + str(act_val) + '___now_time-' + str(time.time()).split('.')[0] +
+            '___exec_time-' + str(dt)[:5] + "sec___v3___" + filename.split('\\')[1])
 
 
 def solve_sparse(a, b):
@@ -115,11 +112,11 @@ def calc_fiedler_vector(filename, act_val):
 
 if __name__ == '__main__':
     dir_name = 'C:\\Users\\Arseny\\Documents\\Prog\\PythonProjects\\MinCut\\Ref'
-    path_list = pathlib.Path(dir_name).glob('**/text_ege.png')
+    path_list = pathlib.Path(dir_name).glob('**/trees_and_sun.jpg')
     for path in path_list:
         file_name = "Ref\\" + str(path).split('\\Ref\\')[1]
 
-        color_activate_value = 15
+        color_activate_value = 10
 
         start_time = time.time()
         fiedler_vec = calc_fiedler_vector(file_name, color_activate_value)
